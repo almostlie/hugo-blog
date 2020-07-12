c88cd566740ca8a19fe8117e95697733a37ebd36
newcome <liukang@snqu.com>
2020-07-12 11:24:40 +0800

    update CNAME

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,6 @@ git remote add origin https://github.com/newcome/newcome.github.com
 
 git push -uf origin master
 
-mv .gitignore ../.gitignorebak
-
 rm -rf ./*
 
 rm -rf ./.git
-
-mv ../.gitignorebak .gitignore
-
